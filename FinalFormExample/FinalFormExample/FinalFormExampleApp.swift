02a44179e9148e962f10ddfe84ff6682f886eff0
Lex Brouwers <ahm_brouwers@hotmail.com>
2025-08-14 16:38:58 +0200

    Add example project

---
+++ FinalFormExample/FinalFormExample/FinalFormExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalFormExampleApp.swift
+//  FinalFormExample
+//
+//  Created by Lex Brouwers on 14/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FinalFormExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinalFormExample/FinalFormExampleTests/FinalFormExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  FinalFormExampleTests.swift
+//  FinalFormExampleTests
+//
+//  Created by Lex Brouwers on 14/08/2025.
+//
+
+import Testing
+
+struct FinalFormExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
